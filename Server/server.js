49dc6a45072fe4e245867cffb5960b1218151336
Priyanshu <priyanshumahawar29@gmail.com>
2023-06-16 00:30:38 +0530

    minor changes

--- Server/server.js
+++ Server/server.js
@@ -11,8 +11,17 @@ const io = new Server(PORT , {
 });
 
 io.on('connection' , socket => {
-    socket.on('send-changes' , delta =>{
-        socket.broadcast.emit('receive-changes',delta); 
-    });  
+
+    socket.on('get-document', documentId => {
+
+        const data = "";
+        socket.join(documentId);
+        socket.emit('load-document' , data);
+        socket.on('send-changes' , delta =>{
+            socket.broadcast.to(documentId).emit('receive-changes',delta); 
+        });
+    })
+
+      
 });
 
