0e742c0df769af467047475cebe9aef974ed23f8
Priyanshu <priyanshumahawar29@gmail.com>
2023-06-15 21:29:39 +0530

    added toolbar

--- Server/client/src/pages/Editor.js
+++ Server/client/src/pages/Editor.js
@@ -1,6 +1,42 @@
+import { Box } from '@mui/material';
+import Quill from 'quill';
+import 'quill/dist/quill.snow.css';
+import { useEffect } from 'react';
+
+
 const Editor = () => {
+
+    const toolbarOptions = [
+        ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+        ['blockquote', 'code-block'],
+      
+        [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+        [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+        [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+        [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+        [{ 'direction': 'rtl' }],                         // text direction
+      
+        [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+        [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+      
+        [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+        [{ 'font': [] }],
+        [{ 'align': [] }],
+      
+        ['clean']                                         // remove formatting button
+      ];
+    
+    useEffect(()=>{
+        const quillServer = new Quill('#container' ,{
+            modules: {
+              toolbar: toolbarOptions
+            }, 
+            theme: 'snow' 
+        });
+    },[]);
+    
     return ( 
-        <h1>Editor</h1>
+        <Box id='container'></Box>
      );
 }
  
