fc3a3528a1581ae345e9425591e74d6797e0224f
Priyanshu <priyanshumahawar29@gmail.com>
2023-06-15 16:53:42 +0530

    Added pages

---
+++ Server/client/src/App.js
@@ -0,0 +1,16 @@
+import Editor from "./pages/Editor";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+
+
+function App() {
+  return (
+    <div>
+      <Home />
+      <Editor />
+      <Login />
+    </div>
+  );
+}
+
+export default App;
